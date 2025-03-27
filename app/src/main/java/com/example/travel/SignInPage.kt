9777be33040c56b0c10ae7c6448855238cdd5e0d
KalharaDMR <It23139626@my.sliit.lk>
2025-03-27 14:53:41 +0800

    Initial Commit update2

--- app/src/main/java/com/example/travel/SignInPage.kt
+++ app/src/main/java/com/example/travel/SignInPage.kt
@@ -1,20 +1,19 @@
 package com.example.travel
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SignInPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sign_in_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val signinbtn2: Button = findViewById(R.id.sign)
+        signinbtn2.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
         }
     }
 }
