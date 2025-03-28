b99ac8dfad06ac9d3e781b79c9b7db136181e248
KalharaDMR <It23139626@my.sliit.lk>
2025-03-28 09:46:58 +0800

    Initial Commit update11

--- app/src/main/java/com/example/travel/SignInPage.kt
+++ app/src/main/java/com/example/travel/SignInPage.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -15,12 +16,30 @@ class SignInPage : AppCompatActivity() {
 
         val editTextName: EditText = findViewById(R.id.editTextText)
         val signinbtn2: Button = findViewById(R.id.sign)
+        val editTextPassword: EditText = findViewById(R.id.editTextTextPassword)
 
-        signinbtn2.setOnClickListener {
-            val name = editTextName.text.toString()
-            val intent = Intent(this, ThirdActivity::class.java)
-            intent.putExtra("USER_NAME", name)
-            startActivity(intent)
-        }
+
+            signinbtn2.setOnClickListener {
+                val name = editTextName.text.toString()
+                val password = editTextPassword.text.toString()
+
+                if (isPasswordValid(password)) {
+                    val intent = Intent(this, ThirdActivity::class.java)
+                    intent.putExtra("USER_NAME", name)
+                    startActivity(intent)
+                }else {
+                    Toast.makeText(
+                        this,
+                        "Password must be at least 8 characters long",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+
+
+    }
+    private fun isPasswordValid(password: String): Boolean {
+        return password.length >= 8
     }
 }
